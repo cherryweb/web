e275481242a8dcc8b647504aa3ddc94958ad32ce
FlameadoDeMoe <agustinruizua@gmail.com>
2014-04-19 18:59:25 +0200

    Añadiendo cads. Ni idea si estan bien.

--- cherryWeb/cherryWebClassLibrary/cherryWebEN.cs
+++ cherryWeb/cherryWebClassLibrary/cherryWebEN.cs
@@ -7,6 +7,8 @@ namespace cherryWebClassLibrary
 {
     public class ENUsuario
     {
+
+        //Propiedades
         private String apodo;
         private String nombre;
         private String password;
@@ -15,6 +17,51 @@ namespace cherryWebClassLibrary
         private String pais;
         private Boolean boletin;
         private String imagen;
+        private String paypal;
+
+        //Datos
+        //private UsuarioCAD m_cc;
+
+        public ENUsuario(string a = "", string n = "", string p = "", string er = "", string ec = "", string ps = "", Boolean b, string i = "", string paypal="")
+        {
+            apodo = a;
+            nombre = n;
+            password = p;
+            er = email_registro;
+            ec = email_contacto;
+            ps = pais;
+            b = boletin;
+            i = imagen;
+            p = paypal;
+        }
+
+        /*public void nuevo_usuario()
+        {
+            try
+            {
+                m_cc = new UsuarioCAD("../webdb");
+            }
+            catch (Error e)
+            {
+                Console.WriteLine("Error creando usuario");
+
+                return;
+            }
+        }
+
+        public void borrar_usuario()
+        {
+            try
+            {
+                m_cc = new UsuarioCAD("../webdb");
+            }
+            catch (Error e)
+            {
+                Console.WriteLine("Error borrando usuario");
+
+                return;
+            }
+        }*/
 
         public String Apodo
         {
@@ -132,21 +179,76 @@ namespace cherryWebClassLibrary
         private int valoracion;
         private String apodo;
         private String nombre;
+
+        private int Valoracion
+        {
+            get { return valoracion; }
+            set { valoracion = value; }
+        }
+
+        private String Apodo
+        {
+            get { return apodo; }
+            set { apodo = value; }
+        }
+
+        private String Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
     }
     public class ENRedesSociales
     {
         private String usuario;
         private String redSocial;
+
+        private String Usuario
+        {
+            get { return usuario; }
+            set { usuario = value; }
+        }
+
+        private String RedSocial
+        {
+            get { return redSocial; }
+            set { redSocial = value; }
+        }
     }
     private class ENDescargas
     {
         private int numerototal;
         private String aplicacion;
+
+        private int Numerototal
+        {
+            get { return numerototal; }
+            set { numerototal = value; }
+        }
+
+        private String Aplicacion
+        {
+            get { return aplicacion; }
+            set { aplicacion = value; }
+        }
     }
     public class ENMediaAplicaciones
     {
         private float media;
         private string aplicacion;
+
+        private float Media
+        {
+            get { return media; }
+            set { media = value; }
+        }
+
+        private String Aplicacion
+        {
+            get { return aplicacion; }
+            set { aplicacion = value; }
+        }
+
     }
 
 }
