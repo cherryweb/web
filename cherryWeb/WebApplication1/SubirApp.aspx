4bb326d28529fbaa609643aad482be3618bd5b7c
SrTopo <raoulmv@gmail.com>
2014-05-10 14:00:02 +0200

    Añadida la pagina SubirApp

---
+++ cherryWeb/WebApplication1/SubirApp.aspx
@@ -0,0 +1,59 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="SubirApp.aspx.cs" Inherits="WebApplication1.SubirApp" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
+    <style type="text/css">
+        .style1
+        {
+        }
+        .style2
+        {
+            width: 325px;
+            height: 86px;
+        }
+        .style3
+        {
+            height: 86px;
+        }
+    </style>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <table style="width:100%;">
+        <tr>
+            <td class="style1">
+                <asp:Label ID="subirapp_nombrelabel" runat="server" Text="Nombre: "></asp:Label>
+                <asp:TextBox ID="subirapp_nombretextbox" runat="server"></asp:TextBox>
+            </td>
+            <td>
+                <asp:Label ID="subirapp_pvp" runat="server" Text="PVP:"></asp:Label>
+                <asp:TextBox ID="subirapp_pvptextbox" runat="server"></asp:TextBox>
+            </td>
+        </tr>
+        <tr>
+            <td class="style2">
+                <asp:Label ID="subirapp_descripcionlaber" runat="server" Text="Descripción:"></asp:Label>
+                <asp:TextBox ID="subirapp_descripciontextbox" runat="server" Height="68px" 
+                    TextMode="MultiLine" Width="238px"></asp:TextBox>
+            </td>
+            <td class="style3">
+                <asp:Label ID="subirapp_cargarimagenlabel" runat="server" Text="Cargar imagen:"></asp:Label>
+                <asp:FileUpload ID="FileUpload1" runat="server" />
+            </td>
+        </tr>
+        <tr>
+            <td class="style1">
+                <asp:Label ID="subirapp_categorialabel" runat="server" Text="Categoría:"></asp:Label>
+                <asp:DropDownList ID="DropDownList1" runat="server">
+                </asp:DropDownList>
+            </td>
+            <td>
+                <asp:Label ID="subirapp_rutaapplabel" runat="server" Text="Ruta App:"></asp:Label>
+                <asp:FileUpload ID="FileUpload2" runat="server" />
+            </td>
+        </tr>
+        <tr>
+            <td class="style1" colspan="2">
+                <asp:Button ID="subirapp_subirboton" runat="server" Font-Bold="True" 
+                    style="text-align: center" Text="SUBIR" />
+            </td>
+        </tr>
+    </table>
+</asp:Content>
---
+++ cherryWeb/WebApplication1/SubirApp.aspx.designer.cs
@@ -0,0 +1,132 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace WebApplication1 {
+    
+    
+    public partial class SubirApp {
+        
+        /// <summary>
+        /// Control subirapp_nombrelabel.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label subirapp_nombrelabel;
+        
+        /// <summary>
+        /// Control subirapp_nombretextbox.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox subirapp_nombretextbox;
+        
+        /// <summary>
+        /// Control subirapp_pvp.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label subirapp_pvp;
+        
+        /// <summary>
+        /// Control subirapp_pvptextbox.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox subirapp_pvptextbox;
+        
+        /// <summary>
+        /// Control subirapp_descripcionlaber.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label subirapp_descripcionlaber;
+        
+        /// <summary>
+        /// Control subirapp_descripciontextbox.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox subirapp_descripciontextbox;
+        
+        /// <summary>
+        /// Control subirapp_cargarimagenlabel.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label subirapp_cargarimagenlabel;
+        
+        /// <summary>
+        /// Control FileUpload1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.FileUpload FileUpload1;
+        
+        /// <summary>
+        /// Control subirapp_categorialabel.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label subirapp_categorialabel;
+        
+        /// <summary>
+        /// Control DropDownList1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList1;
+        
+        /// <summary>
+        /// Control subirapp_rutaapplabel.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label subirapp_rutaapplabel;
+        
+        /// <summary>
+        /// Control FileUpload2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.FileUpload FileUpload2;
+        
+        /// <summary>
+        /// Control subirapp_subirboton.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button subirapp_subirboton;
+    }
+}
