b0e88f0d1ef7913a91635da22698b1e69391a8af
FlameadoDeMoe <agustinruizua@gmail.com>
2014-05-11 10:52:28 +0200

    Capturas para la entrega del día 11

---
+++ cherryWeb/WebApplication1/Aplicaostias.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Title="Aplicaciones" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true"
+    CodeBehind="Aplicaostias.aspx.cs" Inherits="WebApplication1.About" %>
+
+<asp:Content ID="BodyContent" runat="server" ContentPlaceHolderID="MainContent">
+    <p align="center">
+        <asp:Label ID="BuscarAplicacion" runat="server" Text="Buscar por: "></asp:Label> &nbsp;&nbsp;&nbsp;
+        <asp:DropDownList ID="CategoriasAplicacion" runat="server">
+            <asp:ListItem>Categoria</asp:ListItem>
+            <asp:ListItem>Pago</asp:ListItem>
+            <asp:ListItem>Gratuitas</asp:ListItem>
+</asp:DropDownList>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="BusquedaAplicacion" runat="server" Width="164px"></asp:TextBox>
+    </p>
+    <p align="center">
+        <asp:Label ID="LabelNombreApp" runat="server" Text="Nombre de la App: "></asp:Label>    
+        &nbsp;&nbsp;&nbsp;    
+        <asp:TextBox ID="TextNombreAPP" runat="server"></asp:TextBox>
+        </p>
+        <p style="background-color: #FFFFFF">
+            <asp:Image ID="ImageApp" runat="server" />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Label ID="LDescripApp" runat="server" Text="Descripción: "></asp:Label>
+            &nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="TDescripApp" runat="server" Height="35px" Width="294px"></asp:TextBox>
+         &nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button1" runat="server" Text="Ver App" Width="98px" />
+         </p>
+         <p>
+            
+            </p>
+</asp:Content>
+
---
+++ cherryWeb/WebApplication1/Aplicaostias.aspx.designer.cs
@@ -0,0 +1,96 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace WebApplication1 {
+    
+    
+    public partial class About {
+        
+        /// <summary>
+        /// Control BuscarAplicacion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label BuscarAplicacion;
+        
+        /// <summary>
+        /// Control CategoriasAplicacion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList CategoriasAplicacion;
+        
+        /// <summary>
+        /// Control BusquedaAplicacion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox BusquedaAplicacion;
+        
+        /// <summary>
+        /// Control LabelNombreApp.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LabelNombreApp;
+        
+        /// <summary>
+        /// Control TextNombreAPP.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextNombreAPP;
+        
+        /// <summary>
+        /// Control ImageApp.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image ImageApp;
+        
+        /// <summary>
+        /// Control LDescripApp.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LDescripApp;
+        
+        /// <summary>
+        /// Control TDescripApp.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TDescripApp;
+        
+        /// <summary>
+        /// Control Button1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+    }
+}
